8dc22db6fed625756fe6c186095195875757224d
PolozovMichael <polozovmisha.sm.nis.edu.kz@gmail.com>
2022-12-07 20:31:19 +0600

    first commit

--- public/js/index.js
+++ public/js/index.js
@@ -2,102 +2,116 @@ AOS.init({
     disable: 'phone'
 });
 
-$('#first-video').css({
-    "display" : "none"
-});
-$('#second-video').css({
-    "display" : "none"
-});
-$('#third-video').css({
-    "display" : "none"
-});
-$('#fourth-video').css({
-    "display" : "none"
+
+// Popup showreel
+$('.hero__popup__container').hide();
+
+$('.showreel__button').click(()=> {
+    $('.hero__popup__container').slideDown(1000);
+})
+
+$('.close__popup').click(()=> {
+    $('.hero__popup__container').hide(800);
 });
 
-$('#first-col').click(()=> {
+$('#first-video').css({
+    "display": "none"
+});
+$('#second-video').css({
+    "display": "none"
+});
+$('#third-video').css({
+    "display": "none"
+});
+$('#fourth-video').css({
+    "display": "none"
+});
+
+$('#first-col').click(() => {
     $('#first-video').css({
-        "display" : "block",
-        "position" : "absolute",
-        "top" : "0",
-        "left" : "0",
-        "width" : "100%",
-        "height" : "100%",
-        "z-index" : "1000"
+        "display": "block",
+        "position": "absolute",
+        "top": "0",
+        "left": "0",
+        "width": "100%",
+        "height": "100%",
+        "z-index": "1000"
     });
     $('#second-video').css({
-        "display" : "none"
+        "display": "none"
     });
     $('#third-video').css({
-        "display" : "none"
+        "display": "none"
     });
     $('#fourth-video').css({
-        "display" : "none"
+        "display": "none"
     });
 });
-$('#second-col').click(()=> {
+$('#second-col').click(() => {
     $('#second-video').css({
-        "display" : "block",
-        "position" : "absolute",
-        "top" : "0",
-        "left" : "0",
-        "width" : "100%",
-        "height" : "100%",
-        "z-index" : "1000"
+        "display": "block",
+        "position": "absolute",
+        "top": "0",
+        "left": "0",
+        "width": "100%",
+        "height": "100%",
+        "z-index": "1000"
     });
     $('#first-video').css({
-        "display" : "none"
+        "display": "none"
     });
     $('#third-video').css({
-        "display" : "none"
+        "display": "none"
     });
     $('#fourth-video').css({
-        "display" : "none"
+        "display": "none"
     });
-    
+
 });
-$('#third-col').click(()=> {
+$('#third-col').click(() => {
     $('#third-video').css({
-        "display" : "block",
-        "position" : "absolute",
-        "top" : "0",
-        "left" : "0",
-        "width" : "100%",
-        "height" : "100%",
-        "z-index" : "1000"
+        "display": "block",
+        "position": "absolute",
+        "top": "0",
+        "left": "0",
+        "width": "100%",
+        "height": "100%",
+        "z-index": "1000"
     });
     $('#first-video').css({
-        "display" : "none"
+        "display": "none"
     });
     $('#second-video').css({
-        "display" : "none"
+        "display": "none"
     });
     $('#fourth-video').css({
-        "display" : "none"
+        "display": "none"
     });
 });
-$('#fourth-col').click(()=> {
+$('#fourth-col').click(() => {
     $('#fourth-video').css({
-        "display" : "block",
-        "position" : "absolute",
-        "top" : "0",
-        "left" : "0",
-        "width" : "100%",
-        "height" : "100%",
-        "z-index" : "1000"
+        "display": "block",
+        "position": "absolute",
+        "top": "0",
+        "left": "0",
+        "width": "100%",
+        "height": "100%",
+        "z-index": "1000"
     });
     $('#first-video').css({
-        "display" : "none"
+        "display": "none"
     });
     $('#second-video').css({
-        "display" : "none"
+        "display": "none"
     });
     $('#third-video').css({
-        "display" : "none"
+        "display": "none"
     });
 });
 
 $(document).click((e) => {
-    if($(e.target).closest('.col').length === 0)
-        $('.video').css({"display" : "none"});
+    if ($(e.target).closest('.col').length === 0)
+        $('.video').css({
+            "display": "none"
+        });
 });
